cdcbe2e8fcde94bec32417051c6198c7004f77ed
rajesh <rajeshniure567@gmail.com>
2025-08-01 13:18:37 +0545

    clear main

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,15 +1,2 @@
-import axios from "../../axiosConfig";
-
-
-export const fetchProducts = async () => {
-  const res = await axios.get('/products');
-  return res.data;
-};
-
-export const addProduct = async (product: { title: string; description: string; price: number;  image: string; }) => {
-  const res = await axios.post('/products', product);
-  return res.data;
-};
-
 
   
